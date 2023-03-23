7aa337581f091dc01d97b8ae9e08250b21055d96
Yosef Pham <yosefpham03@gmail.com>
2023-03-23 09:41:58 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import './App.scss';
+import Data from './db.json';
 
 function App() {
+  const [title, setTitle] = useState();
+  const [data, setData] = useState(Data.surcharge);
+  const [unit, setUnit] = useState();
+  const [value, setValue] = useState();
+
+  const handleAddSurcharge = () => {
+    if(!title || !unit || !value) {
+      alert("Vui lòng nhập đầy đủ thông tin")
+    } else {
+
+      setData([...data, {
+        title: title,
+        type: unit,
+        value: value
+      }])
+      console.log(data)
+      setTitle("");
+      setUnit("");
+      setValue("");
+    };
+  }
+  const handleDelete = (index) => {
+    let newData = [...data]
+    let id = data.findIndex(x => x.id === index)
+    newData.splice(id, 1)
+    // console.log(newData)
+    setData(newData)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='add-surcharges'>
+          <from>
+            <h5>Tên phụ phí:</h5>
+            <input type="text" className="" placeholder='text' value={title || ''}
+            onChange={e => setTitle(e.target.value)}
+            />
+            <select onChange={e => setUnit(e.target.value)} value={unit}> 
+            <option  value="none">...</option>
+              <option  value="VND">Thành tiền</option>
+              <option value="%">Tỉ lệ</option>
+            </select>
+          </from>
+          <input onChange={e => setValue(e.target.value)} value={value || ''} className={unit === 'VND' ? "input-money display-block" : "input-money"} type="text" placeholder='Đơn vị VNĐ'/>
+          <select onChange={e => setValue(e.target.value)} className={unit === '%' ? "select-percent display-block" : "select-percent"}>
+            <option value="pp1">pp1</option>
+            <option value="pp2">pp2</option>
+            <option value="pp3">pp3</option>
+          </select>
+          
+          <button onClick={handleAddSurcharge}>Thêm</button>
+        </div>
+        <div className="header">
+            <div className="surcharges-header">
+              {/* <h1>Phụ phí</h1>
+              <h1>Thành tiền</h1>
+              <h1>X</h1> */}
+            </div>
+            <div className="surcharges-list">
+              {data.map((data, id) => (
+                <div key={id} className="surcharges">
+                  <h3>{data.title}</h3>
+                  <h3>{data.value} {data.type}</h3>
+                  <h5 onClick={() => handleDelete(id)}>Xoá</h5>
+                </div>
+              ))}
+            </div>
+        </div>
+
       </header>
     </div>
   );
